58465a1265a1ab67baa8c23fae8fbaa7b3ef5697
scoty755 <scoty755@gmail.com>
2015-01-31 12:27:51 +0900

    Fix the problem of product inheritance
    Change-Id: I9385078240c9a433ffb742f4ac2d99a63a2d0466

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/klte/full_kltevzw.mk)
+$(call inherit-product, device/samsung/kltevzw/full_kltevzw.mk)
 
 PRODUCT_DEVICE := kltevzw
 PRODUCT_NAME := cm_kltevzw
